bfce967c124e30d9596fcdec93536141b720bfaa
jill bhatt <bhattjill@gmail.com>
2025-07-11 21:54:59 +0530

    Removed typescript and reset server

--- server/src/app.d.ts
+++
@@ -1,8 +0,0 @@
-import session, { SessionData } from 'express-session';
-
-declare module 'express-session' {
-    interface SessionData {
-        userID: string;
-        username: string;
-    }
-}
